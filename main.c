7927971e95fba3200df04cdb74676a25d8c91e03
LaoP <9493605+renjunyao@user.noreply.gitee.com>
2025-08-27 10:03:08 +0800

    初始化项目

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * @brief 
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
+int sum (int a, int b) {
+    return a + b;
+}
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello World\n");
+
+    return 0;
+}
